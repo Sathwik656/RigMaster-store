d4e557b02577d7e684cedb0d1a3127acd710bd9e
Sathwik <112880525+Sathwik656@users.noreply.github.com>
2025-12-04 20:32:53 +0530

    ao

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -3,7 +3,7 @@ import '../../../envConfig'
 
 const API_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY
 const stripe = new Stripe(API_KEY, {
-    apiVersion: "2025-12-04",
+    apiVersion: "2025-11-17",
 })
 
 export async function POST(request){
