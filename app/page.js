866fe71155c5e0126b34f498ececc0fe7fb29215
Sathwik <112880525+Sathwik656@users.noreply.github.com>
2025-12-04 20:55:38 +0530

    u

--- app/page.js
+++ app/page.js
@@ -2,36 +2,36 @@ import ImageBanner from "@/components/ImageBanner";
 import Products from "@/components/Products";
 
 export async function getProducts() {
-  const baseUrl = process.env.BASE_URL;
+  const baseUrl = process.env.PUBLIC_BASE_URL
+  const response = await fetch(baseUrl + '/api/products', {
+  cache: "no-store"
+})
 
-  const res = await fetch(`${baseUrl}/api/products`, {
-    cache: "no-store"
-  });
-
-  const products = await res.json();
-  return products;
+  const products = await response.json()
+  return products
 }
 
-export default async function Home() {
-  const products = await getProducts();
+export default async function Home(props) {
+  
+  const products = await getProducts()
+  
+  let planner = null
+  let stickers = []
 
-  let planner = null;
-  let stickers = [];
-
-  for (let product of products) {
-    if (product.name === "Intel I9") {
-      planner = product;
-      continue;
+  for(let product of products){
+    if(product.name == 'Intel I9'){
+      planner = product
+      continue
     }
-    stickers.push(product);
+    stickers.push(product)
   }
 
   return (
     <>
-      <ImageBanner />
-      <section>
-        <Products planner={planner} stickers={stickers} />
-      </section>
+    <ImageBanner></ImageBanner>
+    <section>
+       <Products planner={planner} stickers={stickers}></Products>
+    </section>
     </>
   );
 }
